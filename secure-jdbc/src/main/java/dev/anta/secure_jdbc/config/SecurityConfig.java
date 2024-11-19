0a0507125c7b4367156f634a6a59130150bf44da
antarikshgosain <antariksh.gosain@gmail.com>
2024-11-19 10:17:48 -0500

    Configuring users in H2 DB using @Bean EmbeddedDatasource

--- secure-jdbc/src/main/java/dev/anta/secure_jdbc/config/SecurityConfig.java
+++ secure-jdbc/src/main/java/dev/anta/secure_jdbc/config/SecurityConfig.java
@@ -2,13 +2,20 @@ package dev.anta.secure_jdbc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,7 +24,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
+    /*@Bean
     InMemoryUserDetailsManager user() {
         return new InMemoryUserDetailsManager(
                 User.withUsername("user")
@@ -29,11 +36,35 @@ public class SecurityConfig {
                         .roles("ADMIN")
                         .build()
         );
+    }*/
+
+    @Bean
+    EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("dashingboard")
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
     }
+
+
+    @Bean
+    JdbcUserDetailsManager user(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        return jdbcUserDetailsManager;
+    }
+
+
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeRequests(auth -> auth.anyRequest().authenticated())
+        return http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .authorizeRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(header -> header.frameOptions().sameOrigin())
                 .formLogin(withDefaults())
                 .build();
     }
